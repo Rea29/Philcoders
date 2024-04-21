1e7c197799b0edbb0053cdd75dc6a591b3373e01
Rea29 <157829358+Rea29@users.noreply.github.com>
2024-04-21 20:15:22 +0800

    Update App.jsx
    added logtest2

--- src/App.jsx
+++ src/App.jsx
@@ -46,6 +46,7 @@ const App = () => (
       <Route path="/Login" element={<Login />} />
       <Route path="/Logtest" element={<Login />} />
       <Route path="/Logtest1" element={<Login />} />
+      <Route path="/Logtest2" element={<Login />} />
     </Routes>
     <Footer />
   </BrowserRouter>
